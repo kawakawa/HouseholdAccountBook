26692e996fe87427fdc78ddb9c573577325b0e3d
Keisuke Utsumi <void.of.glans@gmail.com>
2012-09-29 14:17:29 +0900

    edit by utsumi create sample project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseholdAccountBook::Application.config.secret_token = '108788dfbb3365669d15d9e4223b005202d397d9fa1c60eaaec2a2955cfc4644d77d02bf14cf2d759c20b5c8e9e8bd448778109b44d73758006ef10bf2638aba'
